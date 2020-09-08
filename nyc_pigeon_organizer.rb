73041fb41f8e01243f52400493ec47f5867b7bb7
SunJet Liu <sunjetliu@gmail.com>
2020-09-08 18:55:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
   # write your code here!
 # plain_hash = { }
 # data.each
-  data.each_with_object({}) do |(key, value), final_array|
+  final_results = data.each_with_object({}) do |(key, value), final_array|
   # binding.pry
     value.each do |inner_key, names|
         names.each do |name|
@@ -16,7 +16,7 @@ def nyc_pigeon_organizer(data)
 if !final_array[name][key]
   !final_array[name][key] = {}
     end
-
+final_array[name][key].push(inner_key)
   final_array
   end
   binding.pry
