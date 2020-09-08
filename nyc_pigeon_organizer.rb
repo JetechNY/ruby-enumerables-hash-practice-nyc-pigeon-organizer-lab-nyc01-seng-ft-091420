bf20e09f2533c769a512e90d3a9e70aee331542b
SunJet Liu <sunjetliu@gmail.com>
2020-09-08 18:56:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,13 +11,14 @@ def nyc_pigeon_organizer(data)
         binding.pry
         if !final_array[name]
           final_array[name] = {}
-
         end
-if !final_array[name][key]
-  !final_array[name][key] = {}
-    end
+        if !final_array[name][key]
+          !final_array[name][key] = {}
+        end
 final_array[name][key].push(inner_key)
   final_array
+end
+end
   end
   binding.pry
 end
